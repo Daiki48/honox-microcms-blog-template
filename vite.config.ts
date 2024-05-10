54631960fabdc4d0024d8d8ea90a06c723c09689
Daiki48 <daiki48.engineer@gmail.com>
2024-05-11 08:23:06 +0900

    :bug: Format

--- vite.config.ts
+++ vite.config.ts
@@ -2,35 +2,32 @@ import pages from '@hono/vite-cloudflare-pages';
 import honox from 'honox/vite';
 import client from 'honox/vite/client';
 import { defineConfig } from 'vite';
-import { getPlatformProxy } from 'wrangler'
+import { getPlatformProxy } from 'wrangler';
 
-export default defineConfig( async ({ mode, command }) => {
+export default defineConfig(async ({ mode, command }) => {
 	if (mode === 'client') {
 		return {
 			plugins: [client()]
 		};
 	} else {
-		if (command === "build") {
+		if (command === 'build') {
 			return {
-				plugins: [honox(), pages()],
+				plugins: [honox(), pages()]
 			};
 		}
 
 		const { env, dispose } = await getPlatformProxy();
 		return {
 			ssr: {
-					external: ['microcms-js-sdk']
-				},
-			plugins: [
-				honox({ devServer: { env, plugins: [{ onServerClose: dispose }] } }),
-				pages(),
-			],
+				external: ['microcms-js-sdk']
+			},
+			plugins: [honox({ devServer: { env, plugins: [{ onServerClose: dispose }] } }), pages()],
 			build: {
 				target: 'esnext'
 			},
 			server: {
-				port: 5173,
-			},
+				port: 5173
+			}
 		};
-	} 
+	}
 });
