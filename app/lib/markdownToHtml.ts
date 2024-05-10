77ebbbc4f27d6feb8108e2b6a071f6ccb8509ef9
Daiki48 <daiki48.engineer@gmail.com>
2024-05-10 23:30:23 +0900

    :truck: Change runtime bun from nodejs

--- app/lib/markdownToHtml.ts
+++ app/lib/markdownToHtml.ts
@@ -1,8 +1,10 @@
-import { promises as fs } from 'node:fs';
+// import { promises as fs } from 'node:fs';
 import { marked } from 'marked';
 
 export const convertToHtml = async (markdown: string) => {
-	const content = await fs.readFile(markdown, 'utf-8');
+	// const content = await fs.readFile(markdown, 'utf-8');
+	const file = Bun.file(markdown);
+	const content = await file.text();
 	const html = marked(content);
 	return html;
 };
