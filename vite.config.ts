7e38caa07f24cb29b23c1488b9f6421e22ab6fac
Daiki48 <daiki48.engineer@gmail.com>
2024-05-10 23:52:31 +0900

    :wrench: Update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,37 @@ import pages from '@hono/vite-cloudflare-pages';
 import honox from 'honox/vite';
 import client from 'honox/vite/client';
 import { defineConfig } from 'vite';
+import { getPlatformProxy } from 'wrangler'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig( async ({ mode, command }) => {
 	if (mode === 'client') {
 		return {
 			plugins: [client()]
 		};
-	} else {
-		return {
-			ssr: {
+	} 
+
+	  // ref: https://github.com/honojs/honox/issues/109
+  if (command === "build") {
+    return {
+      plugins: [honox(), pages()],
+    };
+  }
+
+	  // ref: https://github.com/honojs/honox/issues/39
+  const { env, dispose } = await getPlatformProxy();
+  return {
+		ssr: {
 				external: ['microcms-js-sdk']
 			},
-			plugins: [honox(), pages()],
-			build: {
-				target: 'esnext'
-			}
-		};
-	}
+    plugins: [
+      honox({ devServer: { env, plugins: [{ onServerClose: dispose }] } }),
+      pages(),
+    ],
+		build: {
+			target: 'esnext'
+		},
+    server: {
+      port: 5173,
+    },
+  };
 });
